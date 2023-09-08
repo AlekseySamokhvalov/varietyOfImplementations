16b54def3c170b3b82f2b809294551a96d6f8a82
AlekseySamokhvalov <6560@mail.ru>
2023-09-08 23:50:42 +0300

    Создание проекта

---
+++ src/main/java/com/example/varietyOfImplementations/VarietyOfImplementationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.varietyOfImplementations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarietyOfImplementationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarietyOfImplementationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/varietyOfImplementations/VarietyOfImplementationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.varietyOfImplementations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarietyOfImplementationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
